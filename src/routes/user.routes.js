a23f91191f19c3be4b6608a96d441ab92f5b7df1
KTajerbashi <kamrantajerbashi@gmail.com>
2025-07-04 19:57:54 +0330

    #Feature Add Routes

---
+++ src/routes/user.routes.js
@@ -0,0 +1,8 @@
+// routes/userRoutes.js
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/user_controller");
+
+router.get("/", userController.getUsers);
+
+module.exports = router;
