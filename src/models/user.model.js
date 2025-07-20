789a5a427574c4b05108c90e43752769b5c4c017
KTajerbashi <kamrantajerbashi@gmail.com>
2025-07-20 22:45:11 +0330

    #Fix Some Models Changed

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,4 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
-const validator = require("validator");
+const { validator, bcrypt, mongoose } = require("../global.using");
 
 const userSchema = new mongoose.Schema(
   {
