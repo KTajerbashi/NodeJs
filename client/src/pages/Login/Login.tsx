27843fac988a9a3226556efac1ce0a77931e9fd3
KTajerbashi <kamrantajerbashi@gmail.com>
2026-08-17 22:50:27 +0330

    #Feature Add Auth Pages

---
+++ client/src/pages/Login/Login.tsx
@@ -0,0 +1,13 @@
+function Login() {
+  return (
+    <section>
+      <h1>Login</h1>
+
+      <p>
+        Login page
+      </p>
+    </section>
+  );
+}
+
+export default Login;
---
+++ client/src/pages/SignUp/SignUp.tsx
@@ -0,0 +1,13 @@
+function Signup() {
+  return (
+    <section>
+      <h1>Sign Up</h1>
+
+      <p>
+        Sign up page
+      </p>
+    </section>
+  );
+}
+
+export default Signup;
