a863bdeac5e44ea1813bf2d4c0560675123baa45
KTajerbashi <kamrantajerbashi@gmail.com>
2026-08-23 01:01:35 +0330

    feat: add authentication-related TypeScript interfaces for user management

---
+++ client/src/models/auth/IAuthResponse.d.ts
@@ -0,0 +1,5 @@
+interface IAuthResponse {
+  accessToken: string;
+  user: IAuthUserView;
+  isSuccess: boolean;
+}
---
+++ client/src/models/auth/ISignupDTO.d.ts
@@ -0,0 +1,7 @@
+interface ISignupDTO {
+  key: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
